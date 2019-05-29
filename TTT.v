6665aa07294907c29209e55277d6581d7dd5e687
Greathoney <44860482+Greathoney@users.noreply.github.com>
2019-05-29 10:22:00 +0900

    Update TTT.v

--- TTT.v
+++ TTT.v
@@ -1,3 +1,11 @@
-module TTT(CLOCK, RESET)
+module TTT(clk, rst, key_row, key_col)
+  input clk, rst;
+  input	[3:0]key_row;
+	output [2:0]key_col;
+  reg [11:0]key_data;
+  reg IsItMain, IsItRight;
+  reg [2:0]board[3][3];
+  reg dot_matrix[14][10];
+
 
 endmodule
