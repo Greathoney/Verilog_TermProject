17156092b326ce845dfdd094b6aac884cd027aa3
Greathoney <44860482+Greathoney@users.noreply.github.com>
2019-06-03 19:57:17 +0900

    Update is_three_in_a_row.v

--- is_three_in_a_row.v
+++ is_three_in_a_row.v
@@ -4,17 +4,14 @@ module is_three_in_a_row(board, result);
     output result;
 
     begin
-<<<<<<< HEAD
         if (board[17:16] && board[15:14] && board[13:12]) result = 1;
         else if (board[11:10] && board[9:8] && board[7:6]) result = 1;
         else if (board[5:4] && board[3:2] && board[1:0]) result = 1;
-=======
       if (isTurnO) begin
         // XÀÇ »ï¸ñ ÆÇº°
         if (board[16] && board[14] && board[12]) result = 1;
         else if (board[10] && board[8] && board[6]) result = 1;
         else if (board[4] && board[2] && board[0]) result = 1;
->>>>>>> 813fbc099e85a7bac8d2e2e53915898ee2841bba
 
         else if (board[17:16] && board[11:10] && board[5:4]) result = 1;
         else if (board[15:14] && board[9:8] && board[3:2]) result = 1;
