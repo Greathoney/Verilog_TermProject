884bfc8c5831176746abd41006a242f27b171b3c
Dohun Kim <yh08037@gmail.com>
2019-06-03 19:58:29 +0900

    완성

--- is_three_in_a_row.v
+++ is_three_in_a_row.v
@@ -1,29 +1,25 @@
-module is_three_in_a_row(board, result);
-//ouput 0 1 2
+module is_three_in_a_row(board, result, isTurnO);
     input [17:0] board;
+    input isTurnO;
     output result;
 
     begin
-        if (board[17:16] && board[15:14] && board[13:12]) result = 1;
-        else if (board[11:10] && board[9:8] && board[7:6]) result = 1;
-        else if (board[5:4] && board[3:2] && board[1:0]) result = 1;
       if (isTurnO) begin
         // XÀÇ »ï¸ñ ÆÇº°
         if (board[16] && board[14] && board[12]) result = 1;
         else if (board[10] && board[8] && board[6]) result = 1;
         else if (board[4] && board[2] && board[0]) result = 1;
 
-        else if (board[17:16] && board[11:10] && board[5:4]) result = 1;
-        else if (board[15:14] && board[9:8] && board[3:2]) result = 1;
-        else if (board[13:12] && board[7:6] && board[1:0]) result = 1;
+        else if (board[16] && board[10] && board[4]) result = 1;
+        else if (board[14] && board[8] && board[2]) result = 1;
+        else if (board[12] && board[6] && board[0]) result = 1;
 
-        else if (board[17:16] && board[9:8] && board[1:0]) result = 1;
-        else if (board[13:12] && board[9:8] && board[5:4]) result = 1;
+        else if (board[16] && board[8] && board[0]) result = 1;
+        else if (board[12] && board[8] && board[4]) result = 1;
 
         else result = 0;
       end
       else begin
-        // OÀÇ »ï¸ñ ÆÇº°
         if (board[17] && board[15] && board[13]) result = 1;
         else if (board[11] && board[9] && board[7]) result = 1;
         else if (board[5] && board[3] && board[1]) result = 1;
