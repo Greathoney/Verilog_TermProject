0c4e31278b32d7a88ba0723359c17604c52e2c05
Greathoney <44860482+Greathoney@users.noreply.github.com>
2019-06-03 21:21:04 +0900

    Update

--- gameState.v
+++ gameState.v
@@ -6,12 +6,12 @@ module gaimState(clk, key_data, IsMain, IsTurnO, board, seg_txt, seg_com, dot_co
 2. 순서진행상황에 따라 7-segment에 P1, P2를 띄워야 함.
 */
 
-  	input clk;
-  	input [3:0]key_data;
-  	input IsMain;
-  	inout IsTurnO;
-  	inout [18:0] board;
-    inout [1:0] result;
+  input clk;
+  input [3:0]key_data;
+	input IsMain;
+	inout IsTurnO;
+	inout [18:0] board;
+  inout [1:0] result;
 	output [6:0] seg_txt;
 	output [7:0] seg_com;
 	output dot_col;
@@ -24,16 +24,16 @@ module gaimState(clk, key_data, IsMain, IsTurnO, board, seg_txt, seg_com, dot_co
 	reg [17:0] board;
   reg result; // 00 : 진행중   01 : X승   10 : O승   11 : 무승부
 
-  	always @(posedge clk) begin // clk1 설계
-  		if (IsMain == 1) begin
-  	  	  if (clk_count >= 24999) begin
-  	      	clk_count <= 0;
-  	     	clk1 <= 1;
-  	  	  end
-  	    else begin
-  	      clk_count <= clk_count + 1;
-  	      clk1 <= 0;
-  	    end
+  always @(posedge clk) begin // clk1 설계
+  	if (IsMain == 0) begin
+  	   if (clk_count >= 24999) begin
+  	    	clk_count <= 0;
+  	    	clk1 <= 1;
+      end
+      else begin
+        clk_count <= clk_count + 1;
+	      clk1 <= 0;
+	    end
     end
 	end
 
@@ -42,18 +42,18 @@ module gaimState(clk, key_data, IsMain, IsTurnO, board, seg_txt, seg_com, dot_co
 		else sel_seg <= sel_seg + 1;
 	end
 
-  	always @(IsTurnO) begin
+  always @(IsTurnO) begin
     if(IsMain==0)begin
-  		if (IsTurnO == 1) begin
-  			//7-segment에 P2를 표시하게 된다.
-			case(sel_seg)
-				0: begin seg_com <= 8'b01111111; seg_txt <= 7'b1110011; end //P => abefg
-				1: begin seg_com <= 8'b10111111; seg_txt <= 7'b1101101; end //2 => abdeg
-			endcase
-		end
+  	 if (IsTurnO == 1) begin
+  		 //7-segment에 P2를 표시하게 된다.
+		   case(sel_seg)
+		      0: begin seg_com <= 8'b01111111; seg_txt <= 7'b1110011; end //P => abefg
+			    1: begin seg_com <= 8'b10111111; seg_txt <= 7'b1101101; end //2 => abdeg
+		  endcase
+	   end
 
-  		else begin
-  		  //7-segment에 P1을
+  	 else begin
+  		  //7-segment에 P1을 표시하게 된다.
 			case(sel_seg)
 				0: begin seg_com <= 8'b01111111; seg_txt <= 7'b1110011; end //P => abefg
 				1: begin seg_com <= 8'b10111111; seg_txt <= 7'b0000110; end //1 => bc
@@ -107,6 +107,7 @@ module gaimState(clk, key_data, IsMain, IsTurnO, board, seg_txt, seg_com, dot_co
 
     else begin result = 2'b00; end
 
+  
 	//승패가 나오면 그 데이터를 가지고 다른 모듈에서 출력
 	end
 endmodule
