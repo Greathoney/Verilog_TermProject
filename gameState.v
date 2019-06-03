7f175e4cb8710f62c1161f26a113d848d5908edb
Greathoney <44860482+Greathoney@users.noreply.github.com>
2019-06-03 20:48:47 +0900

    jhg

--- gameState.v
+++ gameState.v
@@ -7,11 +7,11 @@ module gameState(clk, key_data, IsMain, IsRight, IsTurnO, board, seg_txt, seg_co
 */
 
   input clk;
-  input [3:0]key_data;
-  input IsMain;
-  inout IsRight;
-  inout IsTurnO;
-  inout [18:0] board;
+	input [3:0]key_data;
+	input IsMain;
+	inout IsRight;
+	inout IsTurnO;
+	inout [18:0] board;
   inout [1:0] result;
 	output [6:0] seg_txt;
 	output [7:0] seg_com;
@@ -26,15 +26,15 @@ module gameState(clk, key_data, IsMain, IsRight, IsTurnO, board, seg_txt, seg_co
   reg result; // 00 : 진행중   01 : X승   10 : O승   11 : 무승부
 
 	always @(posedge clk) begin // clk1 설계
-		if (IsMain == 1) begin
-  	  if (clk_count >= 24999) begin
-      	clk_count <= 0;
-	     	clk1 <= 1;
-  	  end
-	    else begin
-        clk_count <= clk_count + 1;
-  	    clk1 <= 0;
-      end
+	  if (IsMain == 1) begin
+     if (clk_count >= 24999) begin
+	   	clk_count <= 0;
+	   	clk1 <= 1;
+     end
+  	 else begin
+  	      clk_count <= clk_count + 1;
+  	      clk1 <= 0;
+  	    end
     end
 	end
 
@@ -43,18 +43,6 @@ module gameState(clk, key_data, IsMain, IsRight, IsTurnO, board, seg_txt, seg_co
 		else sel_seg <= sel_seg + 1;
 	end
 
-<<<<<<< HEAD
-	always @(IsTurnO) begin
-		if (IsTurnO == 1) begin
-			//7-segment에 P2를 표시하게 된다.
-    	case(sel_seg)
-    		0: begin seg_com <= 8'b01111111; seg_txt <= 7'b1110011; end //P => abefg
-    		1: begin seg_com <= 8'b10111111; seg_txt <= 7'b1101101; end //2 => abdeg
-    	endcase
-    end
-  	else begin
-  		  //7-segment에 P1을 표시하게 된다.
-=======
   	always @(IsTurnO) begin
   		if (IsTurnO == 1) begin
   			//7-segment에 P2를 표시하게 된다.
@@ -65,8 +53,7 @@ module gameState(clk, key_data, IsMain, IsRight, IsTurnO, board, seg_txt, seg_co
 		end
 
   		else begin
-  		  //7-segment에 P1을 
->>>>>>> 0de98acb514cc92b61b750bce47e9c19023deaf9
+  		  //7-segment에 P1을 표시하게 된다.
 			case(sel_seg)
 				0: begin seg_com <= 8'b01111111; seg_txt <= 7'b1110011; end //P => abefg
 				1: begin seg_com <= 8'b10111111; seg_txt <= 7'b0000110; end //1 => bc
