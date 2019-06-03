636f3046598218b64f839314955570a96b1eb234
Dohun Kim <yh08037@gmail.com>
2019-06-03 20:38:26 +0900

    add result to gameState.v

--- gameState.v
+++ gameState.v
@@ -62,42 +62,50 @@ module gameState(clk, key_data, IsMain, IsRight, IsTurnO, board, seg_txt, seg_co
 	end
 
 
-  	always @(posedge key_data) begin
-        board[18 - 2 * key_data + IsTurnO] = 1;
-  	end
+	always @(posedge key_data) begin
+      board[18 - 2 * key_data + IsTurnO] = 1;
+	end
 
-  		always @(board) begin
-  		//3목을 판별하는 알고리즘
-  		//IsTurnO를 이용한다.
-        if (isTurnO) begin
-          // X의 삼목 판별
-          if (board[16] && board[14] && board[12]) result = 2'b01;
-          else if (board[10] && board[8] && board[6]) result = 2'b01;
-          else if (board[4] && board[2] && board[0]) result = 2'b01;
+	always @(board) begin
+	//3목을 판별하는 알고리즘
+	//IsTurnO를 이용한다.
+    if (isTurnO) begin
+      // X의 삼목 판별
+      if (board[16] && board[14] && board[12]) result = 2'b01;
+      else if (board[10] && board[8] && board[6]) result = 2'b01;
+      else if (board[4] && board[2] && board[0]) result = 2'b01;
 
-          else if (board[16] && board[10] && board[4]) result = 2'b01;
-          else if (board[14] && board[8] && board[2]) result = 2'b01;
-          else if (board[12] && board[6] && board[0]) result = 2'b01;
+      else if (board[16] && board[10] && board[4]) result = 2'b01;
+      else if (board[14] && board[8] && board[2]) result = 2'b01;
+      else if (board[12] && board[6] && board[0]) result = 2'b01;
 
-          else if (board[16] && board[8] && board[0]) result = 2'b01;
-          else if (board[12] && board[8] && board[4]) result = 2'b01;
+      else if (board[16] && board[8] && board[0]) result = 2'b01;
+      else if (board[12] && board[8] && board[4]) result = 2'b01;
+    end
+    else begin
+      // O의 삼목 판별
+      if (board[17] && board[15] && board[13]) result = 2'b10;
+      else if (board[11] && board[9] && board[7]) result = 2'b10;
+      else if (board[5] && board[3] && board[1]) result = 2'b10;
 
-          else result = 0;
-        end
-        else begin
-          if (board[17] && board[15] && board[13]) result = 2'b10;
-          else if (board[11] && board[9] && board[7]) result = 2'b10;
-          else if (board[5] && board[3] && board[1]) result = 2'b10;
+      else if (board[17] && board[11] && board[5]) result = 2'b10;
+      else if (board[15] && board[9] && board[3]) result = 2'b10;
+      else if (board[13] && board[7] && board[1]) result = 2'b10;
 
-          else if (board[17] && board[11] && board[5]) result = 2'b10;
-          else if (board[15] && board[9] && board[3]) result = 2'b10;
-          else if (board[13] && board[7] && board[1]) result = 2'b10;
+      else if (board[17] && board[9] && board[1]) result = 2'b10;
+      else if (board[13] && board[9] && board[5]) result = 2'b10;
+    end
 
-          else if (board[17] && board[9] && board[1]) result = 2'b10;
-          else if (board[13] && board[9] && board[5]) result = 2'b10;
+    // board가 꽉 채워졌는지 판별
+    if ((board[17]|board[16]) & (board[15]|board[14]) & (board[13]|board[12]) &
+      (board[11]|board[10]) & (board[9]|board[8]) & (board[7]|board[6]) &
+      (board[5]|board[4]) & (board[3]|board[2]) & (board[1]|board[0]))
+      begin
+        result = 2'b11;
+      end
 
-          else result = 0;
-        end
-  		//승패가 나오면 그 데이터를 가지고 다른 모듈에서 출력
-  		end
-  endmodule
+    else begin result = 2'b00; end;
+
+	//승패가 나오면 그 데이터를 가지고 다른 모듈에서 출력
+	end
+endmodule
