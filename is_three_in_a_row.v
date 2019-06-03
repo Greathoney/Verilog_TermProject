813fbc099e85a7bac8d2e2e53915898ee2841bba
Dohun Kim <yh08037@gmail.com>
2019-06-03 19:52:09 +0900

    update 삼목판별

--- is_three_in_a_row.v
+++ is_three_in_a_row.v
@@ -20,7 +20,18 @@ module is_three_in_a_row(board, result, isTurnO);
         else result = 0;
       end
       else begin
-        ;
+        if (board[17] && board[15] && board[13]) result = 1;
+        else if (board[11] && board[9] && board[7]) result = 1;
+        else if (board[5] && board[3] && board[1]) result = 1;
+
+        else if (board[17] && board[11] && board[5]) result = 1;
+        else if (board[15] && board[9] && board[3]) result = 1;
+        else if (board[13] && board[7] && board[1]) result = 1;
+
+        else if (board[17] && board[9] && board[1]) result = 1;
+        else if (board[13] && board[9] && board[5]) result = 1;
+
+        else result = 0;
       end
     end
 endmodule
